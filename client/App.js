73cfc4e5353a1d9ec2c803d43fbc3536207a72df
Alexander Pajukoski Soto <alexander.pajukoskisoto@elev.ga.ntig.se>
2024-01-27 14:15:03 +0100

    Done

--- client/App.js
+++ client/App.js
@@ -2,12 +2,22 @@ import * as React from "react";
 import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import moment from "moment/moment";
 import News from "./sites/news";
 import Weather from "./sites/Weather";
 import NewsInfo from "./sites/newsInfo";
 
 function HomeScreen({ navigation }) {
 	const [news, setNews] = React.useState([]);
+	const [weatherData, setWeatherData] = React.useState([]);
+	const [dateTime, setDateTime] = React.useState([""]);
+
+	setInterval(() => {
+		{
+			setDateTime(moment().format("MMMM Do YYYY, h:mm:ss a").toString());
+		}
+	}, 900);
+
 	React.useEffect(() => {
 		fetch(
 			"https://api.nytimes.com/svc/topstories/v2/world.json?api-key=edlvYvl7tG1vz8mWjgk9iW6HuTA1fLCD",
@@ -15,23 +25,29 @@ function HomeScreen({ navigation }) {
 			.then((res) => res.json())
 			.then((res) => setNews(res.results));
 	}, []);
+	React.useEffect(() => {
+		fetch(
+			"https://api.openweathermap.org/data/2.5/weather?lat=59.2293827&lon=17.9748815&units=metric&appid=cfdeb26907457c26a1360e06821fc8b8",
+		)
+			.then((res) => res.json())
+			.then((res) => {
+				setWeatherData(res);
+			});
+	}, []);
 
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>{dateTime}</Text>
 			<Text>Home Screen</Text>
 			<Button
 				title="Go to News"
-				onPress={() =>
-					navigation.navigate(
-						"News",
-						{ news: news },
-						{ navigation: navigation },
-					)
-				}
+				onPress={() => navigation.navigate("News", { news: news })}
 			/>
 			<Button
 				title="Go to Weather"
-				onPress={() => navigation.navigate("Weather")}
+				onPress={() =>
+					navigation.navigate("Weather", { weatherData: weatherData })
+				}
 			/>
 		</View>
 	);
--- client/sites/Weather.js
+++ client/sites/Weather.js
@@ -1,12 +1,33 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function Weather() {
+export default function Weather({ navigation, route }) {
+	const weather = route.params.weatherData;
+	const iconPart1 = "https://openweathermap.org/img/wn/";
+	const iconPart2 = "@4x.png";
 	return (
 		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-			<Text>Weather Screen</Text>
+			<Text>
+				{weather.name},{weather.sys.country}
+			</Text>
+			<Text>{weather.main.temp}&deg;C</Text>
+			<Text>
+				{weather.main.temp_min}&deg;C to {weather.main.temp_max}&deg;C
+			</Text>
+			<Image
+				source={{
+					uri: iconPart1 + weather.weather[0].icon + iconPart2,
+				}}
+				style={{
+					width: 200,
+					height: 200,
+				}}
+			/>
+			<Text>{weather.weather[0].description}</Text>
+			<Text>{weather.wind.speed}m/s</Text>
+			<Text>{weather.main.humidity}%</Text>
 		</View>
 	);
 }
